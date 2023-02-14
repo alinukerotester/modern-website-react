e3636450f938bc383d412a2ee6891bebb7eac87f
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2023-02-14 17:48:23 +0200

    up

--- webcom/pages/_app.js
+++ webcom/pages/_app.js
@@ -1,5 +1,17 @@
 import '@/styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
+
+export default MyApp;
